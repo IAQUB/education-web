f0f7f6970b70895a3e623dab952268945aca3d0b
IAQUB <iqbishaq50@gmail.com>
2025-09-13 00:00:23 +0300

    footer added

--- src/App.jsx
+++ src/App.jsx
@@ -295,7 +295,7 @@ export default function App() {
 
       {/* left side */}
       <div className="pl-10">
-        <button className="bg-[#F5F5F5] border-0 rounded py-3 px-16 shadow text-[#0B7077] my-4">
+        <button className="bg-[#F5F5F5] border-0 rounded py-2 px-7 shadow text-[#0B7077] my-4">
             Benefits
           </button>
 
@@ -332,10 +332,207 @@ export default function App() {
         <img src="https://i.ibb.co/vCF3kXFz/Group-2.png" alt="" />
       </div>
       </div>
-      01025321801
-      01105198455
-      rtayoubur@gmail.com
+    </section>
+
+
+      {/* Staff training */}
+    <section className="grid grid-cols-2 p-14">
+
+
+      {/* left side */}
+         <div className="pr-10">
+        <div className="flex items-center justify-center py-7 gap-5 rounded-xl">
+        <img src="https://i.ibb.co/NgtcQtGW/pana.png" alt="" />
+      </div>
+      </div>
+
+        {/* right side */}
+      <div className="pl-10">
+        <button className="bg-[#D4E1FF] border-0 rounded py-1 px-5 shadow text-[#0B7077] my-4">
+            Training
+          </button>
+          
+          <p className="Inter font-bold text-[48px] text-[#FD661F]">Staff training</p>
+
+          <div className="flex items-center justify-start gap-4 my-5">
+            <div className="bg-[#fafafa] rounded-full p-3 w-fit shadow-xl">
+            <img className="" src="https://i.ibb.co/fzvZYzgY/Group-72.png" alt="" />
+          </div>
+          <p className="text-[#696984]">Teachers don’t get lost in the grid view and have a <br /> dedicated Podium space.</p>
+          </div>
+
+          <div className="flex items-center justify-start gap-4 my-5">
+            <div className="bg-[#fafafa] rounded-full p-3 w-fit shadow-xl">
+            <img className="" src="https://i.ibb.co/TM8THcdW/Group-73.png" alt="" />
+          </div>
+          <p className="text-[#696984]">TA’s and presenters can be moved to the front of the class.</p>
+          </div>
+
+          <div className="flex items-center justify-start gap-4 my-5">
+            <div className="bg-[#fafafa] rounded-full p-3 w-fit shadow-xl">
+            <img className="" src="https://i.ibb.co/cK3GtSMR/Vector.png" alt="" />
+          </div>
+          <p className="text-[#696984]">Teachers can easily see all students and class data at one time.</p>
+          </div>
+      </div>
+    </section>
+
+    
+    {/* gift cards */}
+    <section className="">
+      <div className="bg-[url(https://i.ibb.co/8kbzH4Z/Rectangle-2748.png)] bg-no-repeat bg-cover bg-center border rounded-xl mx-20 my-28 relative px-16 pt-8">
+
+      <h1 className="Inter font-bold text-[48px] text-white py-3">Why You should buy <br /> gift cards ?</h1>
+
+         <div className="flex items-center justify-start gap-1 py-1">
+          <img className="w-6" src="https://i.ibb.co/zHCGQf91/mdi-star-circle.png" alt="" />
+          <p className="text-[#FFFFFF]">Teachers don’t get lost in the grid view and have a dedicated Podium space.</p>
+          </div>
+
+         <div className="flex items-center justify-start gap-1 py-1">
+          <img className="w-6" src="https://i.ibb.co/zHCGQf91/mdi-star-circle.png" alt="" />
+          <p className="text-[#FFFFFF]">Teachers don’t get lost in the grid view and have a dedicated Podium space.</p>
+          </div>
+
+         <div className="flex items-center justify-start gap-1 pt-1 pb-5">
+          <img className="w-6" src="https://i.ibb.co/zHCGQf91/mdi-star-circle.png" alt="" />
+          <p className="text-[#FFFFFF]">Teachers don’t get lost in the grid view and have a dedicated Podium space.</p>
+          </div>
+          <button className="border-0 rounded-md py-3 px-8 shadow bg-white text-[#0B7077] Railway mb-6 text-sm">
+           BUY NOW
+          </button>
+
+          <img className="absolute -top-40 -right-10" src="https://i.ibb.co/6JHK3QB2/image-6.png" alt="" />
+         </div>
+
+    </section>
+
+
+    {/* Exclusive Bundles */}
+
+    <section>
+        <div>
+        <div className="flex items-center justify-center flex-col pt-10">
+        <p className="text-[#0B7077] text-5xl font-bold text-center">Exclusive Bundles</p>
+        <img className="" src="https://i.ibb.co/zh2CQ59w/Vector-1.png" alt="" />
+           </div>
+        <p className="inter flex items-center justify-center text-[#696984] text-center py-8">Onlearing  is one powerful online software suite that combines all the tools <br /> needed to run a successful school or office.</p>
+      </div>
+
+      <div className="flex justify-between items-center">
+        {/* 1st Colum */}
+        <div className="">
+          <img className="" src="https://i.ibb.co/mFc33N22/Frame-237693.png" alt="" />
+        </div>
+
+        {/* 2nd Colum */}
+        <div className="flex border rounded-xl w-[820px] h-[302px] mr-20 relative">
+          <img className="rounded-l-xl" src="https://i.ibb.co/XxMJQr9j/Rectangle-2750-1.png" alt="" />
+
+          <div className="pl-6 pr-20 space-y-4 py-8">
+          <p className="Raleway text-sm text-[#777795]"> 1 - 28 July 2022</p>
+          <p className="text-[#0B7077] font-extrabold Raleway text-xl ">Product Management Basic - Course</p>
+          <p className="text-sm text-[#4D4D4D]">Product Management Masterclass, you will learn with Sarah Johnson - Head of Product Customer Platform Gojek Indonesia.</p>
+          </div>
+
+           <div className="flex items-center space-x-2 bg-white border rounded-full absolute p-2 bottom-7 left-56 cursor-pointer">
+              <p className="text-[#FD661F] font-bold text-[17px]">$ 467</p>
+              <p className="line-through">$ 500</p>
+            </div>
+
+
+             <div className="flex items-center justify-center bg-[#FAFAFA] 2xl:bg-[#FAFAFA] border-1 rounded-full py-2 px-4 w-max absolute bottom-5 right-20">
+            <img className="relative z-50 -ml-0" src="https://i.ibb.co/dsGjPGPb/Blink.png" alt="" />
+            <img className="relative z-40 -ml-2" src="https://i.ibb.co/N2vF7syq/Ander.png" alt="" />
+            <img className="relative z-30 -ml-2" src="https://i.ibb.co/pj8zdTmg/Big-Bird.png" alt="" />
+            <img className="relative z-20 -ml-2" src="https://i.ibb.co/XNQrxRd/Bartender.png" alt="" />
+            <img className="relative z-10 -ml-2" src="https://i.ibb.co/DDkxS6H6/Bill.png" alt="" />
+            <p className="Raleway pl-2">+ 342 students</p>
+          </div>
+          
+        </div>
+
+        {/* 3rd Colum */}
+        <div>
+          <img className="blur" src="https://i.ibb.co/spYt0g7J/Rectangle-2750.png" alt="" />
+        </div>
+      </div>
+
+      <div className="flex items-center justify-center -mt-16">
+        <img src="https://i.ibb.co/FbFtjJXs/Frame-237699.png" alt="" />
+      </div>
+
+    </section>
+
+    {/* footer */}
+    <section className="mt-20">
+      <div className="w-full h-[447px] bg-cover bg-[url('https://i.ibb.co/vC8Y4b9Z/Rectangle-2689.png')] bg-no-repeat ">
       
+      <div className="flex items-center justify-around h-full px-28 py-10">
+        {/* 1st Colum */}
+        <div className="">
+        <div className="flex items-center justify-center gap-3 my-10 py-5 -ml-24 -mb-1">
+        <img className="" src="https://i.ibb.co/n2GCC73/Frame-19.png" alt="" />
+        <img className="" src="https://i.ibb.co/d4DTKxQj/Vidhalaya.png" alt="" />
+        </div>
+
+        <div className="">
+        <div className="flex items-center gap-3">
+          <img src="https://i.ibb.co/WW7z86GR/Location.png" alt="" />
+          <p className="text-[#0B7077]">Address:</p>
+        </div>
+        <p className="text-[#0B7077] py-1">Lorem ipsum dolor sit amet, consectetur <br /> adipiscing elit,</p>
+        </div>
+
+        <div className="flex items-center gap-3 py-3">
+          <img className="w-7" src="https://i.ibb.co/7d2HwBJq/Calling.png" alt="" />
+          <p className="text-[#0B7077]">Tel :+9229341037</p>
+        </div>
+        <div className="flex items-center gap-3">
+          <img className="w-7" src="https://i.ibb.co/XdSnwsj/Time-Circle.png" alt="" />
+          <p className="text-[#0B7077]">Response hours: 8 to 20</p>
+        </div>
+        <div className="flex items-center gap-3 py-3">
+          <img className="w-7" src="https://i.ibb.co/6cDmLFw5/Message.png" alt="" />
+          <p className="text-[#0B7077]">Tel :+9229341037</p>
+        </div>
+
+        </div>
+
+
+      {/* Col-2 */}
+      <div className="grid items-center grid-cols-2 gap-8 pl-20 ">
+        <div className="gap-y-6">
+        <p className="text-[#0A033C] py-3 font-bold">Categories</p>
+        <p className="text-[#0B7077]">Counseling</p>
+        <p className="text-[#0B7077] py-2">Health and fitness</p>
+        <p className="text-[#0B7077]">Individual development</p>
+        <p className="text-[#0B7077] py-2">more</p>
+        </div>
+
+        <div className="-mt-20">
+          <p className="text-[#0A033C] py-3 font-bold">Links</p>
+        <p className="text-[#0B7077]">About us</p>
+        <p className="text-[#0B7077]">blog</p>
+        
+        </div>
+        
+        
+      </div>
+
+
+      {/* Col- 3 */}
+
+      <div className="flex items-center justify-center flex-col relative">
+        <p className="text-[#0A033C] py-3 text-center ">Stay up to date with the latest courses</p>
+        <input className="w-96 rounded-xl py-5 px-4 text-[#0B7077] outline-none" type="email" name="Email" id="" placeholder= "Email" />
+        <button className=" py-2 px-8 bg-[#0B7077] text-white rounded-xl absolute bottom-3 right-3">Send</button>
+      </div>
+      </div>
+
+     </div>
+
+
 
     </section>
 
